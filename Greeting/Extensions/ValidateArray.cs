916d8079bda9082f26f9889845e523e1cc41b71b
Doriano De Iaco <dorianblack97@gmail.com>
2022-02-16 13:02:36 +0100

    Refactor ValidateArray class

--- Greeting/Extensions/ValidateArray.cs
+++ Greeting/Extensions/ValidateArray.cs
@@ -7,7 +7,7 @@ namespace Greeting.Extensions
     public static class ValidateArray
     {
 
-        public static string[] Validate(this string[] names)
+        public static string[] Validate2(this string[] names)
         {
             var namesList = new List<string>();
             foreach (var item in names)
@@ -18,8 +18,22 @@ namespace Greeting.Extensions
                 }
                 else namesList.AddRange(item.Split(",", StringSplitOptions.RemoveEmptyEntries));
             }
-
             return namesList.Select(x => x.Trim()).ToArray();
+        }
+
+        public static string[] Validate(this string[] names)
+        {
+            var namesList = names.SelectMany(x => (Test(x)));
+            return namesList.ToArray();
+        }
+
+        private static IEnumerable<string> Test(string input)
+        {
+            if (input.Contains("\""))
+            {
+                return new String[] { input.Trim('"') };
+            }
+            else return input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
         }
 
     }
