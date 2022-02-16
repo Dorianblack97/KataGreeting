883a025e130d2201d3822255f6823af3483ff167
Doriano De Iaco <dorianblack97@gmail.com>
2022-02-16 10:12:19 +0100

    Refactor NullNameGreeting

--- Greeting/Chain/NullNameGreeting.cs
+++ Greeting/Chain/NullNameGreeting.cs
@@ -2,11 +2,7 @@
 {
     public class NullNameGreeting : AbstractGreeting
     {
-        public override string Handle(params string[] names)
-        {
-            if (names == null)
-                return "Hello, my friend.";
-            return (Next == null) ? "Hello, my friend." : Next.Handle(names);
-        }
+        public override string Handle(params string[] names) => names == null ? "Hello, my friend." : Next.Handle(names);
+        
     }
 }
